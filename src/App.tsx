7f9b96a719968d238a76fa1a550e9512c187a2ed
Eric439 <163082812+Eric439@users.noreply.github.com>
2025-05-11 17:58:26 +0900

    Add UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Home from "./pages/Home";
 
 const App = () => {
   const router = createBrowserRouter([
